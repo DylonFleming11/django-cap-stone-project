3792f3863bf2192030685fdb1d12f736836b3afc
DylonFleming11 <Dylonfleming11@yahoo.com>
2021-07-15 15:30:44 -0500

    All backend code is in place! Not 100% sure everything is right, but its time to move to front.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views.player_views import Players
+from .views.player_views import Players, PlayerDetail
 from .views.mango_views import Mangos, MangoDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
   	# Restful routing
     path('players/', Players.as_view(), name='players'),
+    path('players/<int:pk>/', PlayerDetail.as_view(), name='player_detail'),
     path('mangos/', Mangos.as_view(), name='mangos'),
     path('mangos/<int:pk>/', MangoDetail.as_view(), name='mango_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
--- api/views/player_views.py
+++ api/views/player_views.py
@@ -26,7 +26,33 @@ class Players(generics.ListCreateAPIView):
             return Response({ 'player': player.data }, status=status.HTTP_201_CREATED)
         return Response(player.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class PlayerDetail(generics.RetieveUpdateDestoryAPIView):
-    permission_classes(IsAuthenticated,)
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=(IsAuthenticated,)
     def get(self, request, pk):
         """Show request"""
+        player = get_object_or_404(Player, pk=pk)
+        if not request.user.id == player.owner.id:
+            raise PermissionDenied('Unauthorized, you did not make this player')
+
+        data = PlayerSerializer(player).data
+        return Response({ 'player': data })
+
+    def delete(self, request, pk):
+        """Delete request"""
+        player = get_object_or_404(Player, pk=pk)
+        if not request.user.id == player.owner.id:
+            raise PermissionDenied('Unauthorized, you did not make this player')
+        player.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def partial_update(self, request, pk):
+        """Update Request"""
+        player = get_object_or_404(Player, pk=pk)
+        if not request.user.id == player.owner.id:
+            raise PermissionDenied('Unauthorized, you did not make this player')
+        request.data['player']['owner'] = request.user.id
+        data = PlayerSerializer(player, data=request.data['player'], partial=True)
+        if data.id_valid():
+            data.save()
+            return Response()
+        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
